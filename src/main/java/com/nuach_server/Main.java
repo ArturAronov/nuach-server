d203f32d63278a7b5e6334ee8944f14d24abaed4
Artur Aronov <unionco@icloud.com>
2025-06-19 03:03:51 +0800

    wip

--- src/main/java/com/nuach_server/Main.java
+++ src/main/java/com/nuach_server/Main.java
@@ -1,18 +1,20 @@
 package com.nuach_server;
 
+import com.nuach_server.llm.Ollama;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Main {
-
-	@RequestMapping("/")
-	public String helloz() {
-		return "I'm ALIVE!!!!!!!!!";
-	}
+	Ollama ollama;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
