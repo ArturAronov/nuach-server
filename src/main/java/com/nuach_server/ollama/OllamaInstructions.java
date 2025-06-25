39495ce45802c76dda1ba59775e921df5c3b48b4
Artur Aronov <unionco@icloud.com>
2025-06-26 03:01:29 +0800

    add OllamaInstructions

---
+++ src/main/java/com/nuach_server/ollama/OllamaInstructions.java
@@ -0,0 +1,19 @@
+package com.nuach_server.ollama;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OllamaInstructions {
+    public String getAnswerPrompt(String input) {
+        return "You are examining someone who is practicing for an exam or job interview. \n" +
+                "- The user will provide you with a topic to practice, or a book to rehearse. \n" +
+                "- Provide **1** question only. \n" +
+                "- Do **not** provide answers—your questions must allow for open-ended responses from the user. You will evaluate their answers later. \n" +
+                "- Always provide questions using this exact format: { 'question': '<your question here>' }. Do not use any other format. \n" +
+                "- Only provide questions as described above. \n" +
+                "- Do **not** answer user questions, respond to casual conversation, or perform any off-topic tasks. \n" +
+                "- If the user requests any of these actions, respond with: { 'error': 'This action cannot be executed' }. \n" +
+                "Here is the user's query: \n" +
+                input;
+    }
+}
