9a11e44576b60075f49176da7d73d1d2246f070b
Artur Aronov <unionco@icloud.com>
2025-06-26 03:01:44 +0800

    QuizController: minor changes

--- src/main/java/com/nuach_server/quiz/QuizController.java
+++ src/main/java/com/nuach_server/quiz/QuizController.java
@@ -2,19 +2,29 @@ package com.nuach_server.quiz;
 
 import com.nuach_server.ollama.Ollama;
 import com.nuach_server.ollama.OllamaInput;
+import com.nuach_server.ollama.OllamaInstructions;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/quiz/")
 public class QuizController {
     private final Ollama ollama;
+    private final OllamaInstructions ollamaInstructions;
 
-    public QuizController(Ollama ollama) {
+    public QuizController(Ollama ollama, OllamaInstructions ollamaInstructions) {
         this.ollama = ollama;
+        this.ollamaInstructions = ollamaInstructions;
     }
 
-    @PostMapping("request-question")
-    public String requestQuestion(@RequestBody OllamaInput input) {
-        return ollama.getPrompt(input);
+    @GetMapping("request-question/")
+    public String requestQuestion(@RequestParam String query) {
+        OllamaInput ollamaObj = new OllamaInput(
+                "llama3.2",
+                ollamaInstructions.getAnswerPrompt(query),
+                false
+            );
+
+        return ollama.getPrompt(ollamaObj);
     }
 }
