0fbaecabf8e8f7d2d0329f0214238bf07c0bc980
Artur Aronov <unionco@icloud.com>
2025-06-26 19:38:56 +0800

    minor changes

--- src/main/java/com/nuach_server/ollama/OllamaInstructions.java
+++ src/main/java/com/nuach_server/ollama/OllamaInstructions.java
@@ -9,10 +9,10 @@ public class OllamaInstructions {
                 "- The user will provide you with a topic to practice, or a book to rehearse. \n" +
                 "- Provide **1** question only. \n" +
                 "- Do **not** provide answers—your questions must allow for open-ended responses from the user. You will evaluate their answers later. \n" +
-                "- Always provide questions using this exact format: { 'question': '<your question here>' }. Do not use any other format. \n" +
+                "- Always provide questions using this exact JSON format: { \"question\": \"<your question here>\" }. Do not use any other format. \n" +
                 "- Only provide questions as described above. \n" +
                 "- Do **not** answer user questions, respond to casual conversation, or perform any off-topic tasks. \n" +
-                "- If the user requests any of these actions, respond with: { 'error': 'This action cannot be executed' }. \n" +
+                "- If the user requests any of these actions, respond JSON with: { \"error\": \"This action cannot be executed\" }. \n" +
                 "Here is the user's query: \n" +
                 input;
     }
